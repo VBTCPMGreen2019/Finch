f3b97091358d54df65a8c1d6a55cc41fffa402c8
Taylan Aitken <taitken0010@vbtc.net>
2019-03-25 12:30:55 -0400

    initial commit

---
+++ ControlFinch.py
@@ -0,0 +1,22 @@
+from finch import Finch
+from keyboard import *
+import keyboard
+from time import sleep
+
+finch = Finch()
+while True:
+        if keyboard.is_pressed('w'):
+                finch.wheels(1,1)
+        if keyboard.is_pressed('a'):
+                finch.wheels(-0.7,0.7)
+        if keyboard.is_pressed('d'):
+                finch.wheels(0.7,-0.7)
+        if keyboard.is_pressed('s'):
+                finch.wheels(-1,-1)
+        if keyboard.is_pressed('q'):
+                finch.sleep(3)
+        if keyboard.is_pressed('e'):
+                 break
+                
+finch.close()
+
